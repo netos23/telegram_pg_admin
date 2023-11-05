c1595f87e8adff3af5a5ee5d14472dde698b050e
JohnyTwoJacket <hagoga.pr@gmail.com>
2023-11-05 05:22:32 +0300

    add chat_id user

--- bot-service/models.py
+++ bot-service/models.py
@@ -10,6 +10,7 @@ class UrlConnection(db.Model):
     id: int = db.Column(db.Integer, primary_key=True)
     name: str = db.Column(db.String(80), nullable=False)
     tg_user_id: str = db.Column(db.String(80), nullable=False)
+    tg_chat_id: str = db.Column(db.String(80), nullable=False)
     url: str = db.Column(db.String(120), nullable=False)
     api_key: str = db.Column(db.String(120), unique=True, nullable=True)
     is_active: bool = db.Column(db.Boolean, nullable=True)
