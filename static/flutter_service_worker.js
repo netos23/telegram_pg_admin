911339395d6e15ed9861efca3c5564157ec4e6c3
netos23 <nikmoroz2002@outlook.com>
2023-11-05 03:40:29 +0300

    update web

--- static/flutter_service_worker.js
+++ static/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "c2a08cda2a1e690bfed5ce9fc4d9e5b1",
-"index.html": "3424035a736d551b46029ed804cef7e5",
-"/": "3424035a736d551b46029ed804cef7e5",
-"main.dart.js": "1e3dd78569bb4c5f25af1c048cddf6ca",
+"index.html": "0a45657115a1bd2af6b9ca5c07e815be",
+"/": "0a45657115a1bd2af6b9ca5c07e815be",
+"main.dart.js": "99a46a1ab4c5918ea7fa826d7ea76649",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
